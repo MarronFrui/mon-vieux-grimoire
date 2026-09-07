60a8b873938737bde159bda3bf418d602e81603b
MarronFrui <maxime.fels@gmail.com>
2026-09-07 18:52:47 +0200

    Feat : add a middleware to avoid CORS errors

--- backend/app.js
+++ backend/app.js
@@ -3,7 +3,26 @@ const express = require('express');
 const app = express();
 
 app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization',
+  );
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+  next();
+});
+
+app.use('/api/books', (req, res, next) => {
+  const books = [
+    {
+      title: 'book1',
+    },
+    {
+      title: 'book2',
+    },
+  ];
   res.locals.someKey = 'Requete reçue !';
+  res.status(200).json(books);
   next();
 });
 
@@ -11,10 +30,6 @@ app.use((req, res, next) => {
   res.status(201);
   res.json({ message: `${res.locals.someKey}` });
   next();
-});
-
-app.use((req, res) => {
-  return;
 });
 
 module.exports = app;
