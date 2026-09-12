2a100bf99d73e0a23ec75e4afdb9781e341d346e
MarronFrui <maxime.fels@gmail.com>
2026-09-12 03:21:17 +0200

    Architecture change: /controlers will hold the middlewares while /routes is declarative

--- backend/controler/createBook.js
+++ backend/controler/createBook.js
@@ -1,3 +1,7 @@
-// export function createBook(req, res, next()) {
-//   return
-// }
+module.exports = function createBook(req, res, next) {
+  const Thing = require('./models/Thing');
+  const thing = new Thing({
+    ...req.body,
+  });
+  return;
+};
--- backend/routes/books.js
+++ backend/routes/books.js
@@ -1,6 +1,6 @@
 const express = require('express');
 
-const CreateBookCtrl = require('./controler/createBook.js');
+const CreateBookCtrl = require('../controler/createBook.js');
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
