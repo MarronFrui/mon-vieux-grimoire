944761e9379d5e8a0dc02d2d2d65b2cba049b969
MarronFrui <maxime.fels@gmail.com>
2026-09-16 03:46:41 +0200

    Feat : Add schema to build Auth routes

--- backend/controllers/BookCtrl.js
+++ backend/controllers/BookCtrl.js
@@ -1,6 +1,4 @@
-const { isFunctionDeclaration } = require('typescript');
 const Thing = require('../models/Thing.js');
-// const mock = require('../../frontend/public/data/data.json');
 
 module.exports = {
   createBook: function (req, res, _next) {
@@ -15,8 +13,6 @@ module.exports = {
   },
 
   getBooks: function (_req, res, next) {
-    // console.log(mock);
-    // res.status(200).json(mock);
     Thing.find()
       .then((things) => res.status(200).json(things))
       .catch((error) => res.status(400).json({ error }));
